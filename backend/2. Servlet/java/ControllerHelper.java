ef2afe84d552f8986435f7b5c70f46aa04b9f24d
Eonseo <eonseo15@daum.net>
2025-10-09 22:29:29 +0900

    2. Servlet - Front Controller

---
+++ backend/2. Servlet/java/ControllerHelper.java
@@ -0,0 +1,42 @@
+package com.ssafy.live.controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ControllerHelper {
+
+    public default String preProcessing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 로깅
+        System.out.println(req.getRequestURL() + " : " + req.getMethod());
+        System.out.println("파라미터 로깅");
+        Map<String, String[]> params = req.getParameterMap();
+        params.forEach((k, v) -> {
+            System.out.println("key name: %s, values: %s".formatted(k, Arrays.toString(v)));
+        });
+        // 2. action parameter 반환
+        String action = req.getParameter("action");
+        if (action == null || action.isBlank()) {
+            action = "index";
+        }
+        return action;
+    }
+
+    public default void responseHtml(HttpServletResponse resp, String title, String content) throws IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        //  서블릿의 단점: html 을 한 땀 한 땀 !?
+        String html = "<html><body><h1><%s/h1><div>%s</div></body></hjtml>".formatted(title, content);
+        resp.getWriter().write(html);
+    }
+
+    //TODO: 01. redirect와 forward를 처리할 수 있는 utility method로 redirect, forward를 작성하세요.
+    public default void redirect(HttpServletRequest request, HttpServletResponse response, String path) throws IOException {}
+    public default void forward(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException {}
+
+    //END
+}
---
+++ backend/2. Servlet/java/MainController.java
@@ -0,0 +1,80 @@
+package com.ssafy.live.controller;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.List;
+
+import com.ssafy.live.model.service.BasicSimpleService;
+import com.ssafy.live.model.service.SimpleService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("/main")
+public class MainController extends HttpServlet implements ControllerHelper {
+
+    private final SimpleService sService = null;
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String action = preProcessing(req, resp);
+        switch (action) {
+        case "gugu" -> gugu(req, resp);
+        default -> resp.sendError(HttpServletResponse.SC_NOT_FOUND); // 404 처리
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = preProcessing(req, resp);
+        switch (action) {
+            case "login" -> login(req, resp);
+        }
+    }
+
+    protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // 1. 파라미터 추출
+        String id = req.getParameter("id");
+        String password = req.getParameter("pass");
+
+        // 2. 비즈니스 로직 호출
+        String result = null;
+        if ("ssafy".equals(id) && "1234".equals(password)) {
+            result = "로그인 성공!";
+        } else {
+            result = "id/pass 확인";
+        }
+
+        // 3. 화면 처리
+        responseHtml(resp, "로그인 결과", result);
+    }
+
+    private void gugu(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String content = null;
+
+        try {
+            int dan = Integer.parseInt(request.getParameter("dan"));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < 10; i++) {
+                sb.append("<li>%d * %d = %d".formatted(dan, i, dan * i));
+            }
+            content = "<ul>" + sb + "<ul>";
+
+        } catch (NumberFormatException e) {
+            content = e.getMessage();
+        } catch (RuntimeException e) {
+        }
+        responseHtml(response, "구구단", content);
+
+    }
+}
