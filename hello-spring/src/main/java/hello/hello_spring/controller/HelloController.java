bd39b1a1c605d86a58f9b55649065dc69fc5000b
eonseo <vkvnxn15@naver.com>
2025-11-18 17:03:21 +0900

    [spring] 정적 컨텐츠, MVC 와 템플릿 엔진

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,13 +3,24 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
+        // model.addAttribute: 컨트롤러에서 템플릿으로 데이터를 전달하는 방법
+        // "data"="hello!" 저장 (고정값)
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        // http://localhost:8080/hello-mvc?name=spring
+        // -> 모델이 "name"="spring" 저장
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
